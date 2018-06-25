0c073a994e88bb10d0869c8f7837b02e1aaea70b
ztrixack <ztrixack.th@gmail.com>
2018-06-25 15:58:08 +0700

    add basic every some

---
+++ basic-every-some/program.js
@@ -0,0 +1,9 @@
+module.exports = function checkUsersValid(goodUsers) {
+  return function allUsersValid(submittedUsers) {
+    return submittedUsers.every(submittedUser => {
+      return goodUsers.some(goodUser => {
+        return submittedUser.id === goodUser.id
+      })
+    })
+  }
+}
